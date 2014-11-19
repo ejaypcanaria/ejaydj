99f0feb87785530f96f8808dc0a44034f1b75b63
Ejay Canaria <ejaypcanaria@gmail.com>
2014-11-19 23:31:25 +0800

    Add playlist spec with pending tests

---
+++ spec/playlist_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+RSpec.describe Ejaydj::Playlist do
+
+  describe '#next_track' do
+    it "returns the next track in the playlist"
+  end
+
+  describe '#tracks' do
+    it "returns all the playlist's tracks"
+  end
+
+  describe '#reload!' do
+    it "reloads all the tracks"
+  end
+
+end
