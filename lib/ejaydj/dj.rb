94fc7e069cab98a72740ee4f3986475b360edb30
Ejay Canaria <ejaypcanaria@gmail.com>
2014-11-19 19:00:16 +0800

    Implement playing a song based on a scheduled playlists

--- lib/ejaydj/dj.rb
+++ lib/ejaydj/dj.rb
@@ -3,14 +3,37 @@ require 'ejaydj/spotify/client'
 
 module Ejaydj
   class Dj
+    PLAYLIST_SCHEDULE = {
+      6..11   => :morning_playlists,      # 6AM  - 12PM
+      12..17  => :noon_playlists,         # 12PM - 6PM
+      18..22  => :night_playlists,        # 6PM  - 11PM
+      23..5   => :latenight_playlists     # 11PM - 6AM
+    }
+
     attr_accessor :music_client_id,
                   :music_client_secret,
                   :music_user_id,
-                  :music_client
+                  :music_client,
+                  :morning_playlists,
+                  :noon_playlists,
+                  :night_playlists,
+                  :latenight_playlists
+
 
     def initialize(attributes={})
       instantiate_variables_from attributes
       yield self if block_given?
+    end
+
+    def play_me_a_song(time: Time.now)
+      current_playlist(time).next_track
+    end
+
+    def current_playlist(time)
+      playlist_name = scheduled_playlist(time.strftime('%k').to_i)
+      playlist = playlists.select do |playlist|
+        playlist.name == playlist_name
+      end.first
     end
 
     def playlists
@@ -19,17 +42,27 @@ module Ejaydj
 
     def reload!
       @playlists = all_playlists
-      @playlists.each {|playlist| playlist.reload! }
+      @playlists.each {|playlist| playlist.reload!}
     end
 
     private
 
+    def scheduled_playlist(time)
+      scheduled_playlists = PLAYLIST_SCHEDULE.select do |schedule, value|
+        schedule.cover? time
+      end.values.first
+
+      send(scheduled_playlists).sample
+    end
+
     def all_playlists
-      playlist_service.all_from(@music_user_id)
+      playlist_service.all
     end
 
     def playlist_service
-      @playlist_service ||= Services::PlaylistService.new(music_client: music_client)
+      @playlist_service ||= Services::PlaylistService.new(
+                                music_client: music_client,
+                                user_id: music_user_id)
     end
 
     def music_client
